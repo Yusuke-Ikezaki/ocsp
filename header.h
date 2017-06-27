25e2c20d6e6c241dbb6f9fc5d777a1f20d64f97b
Yusuke Ikezaki <iappley08@gmail.com>
2017-06-28 02:08:07 +0900

    edited myshell.c

--- header.h
+++ header.h
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <dirent.h>
 #include <time.h>
-#include <string.h>
 #include <unistd.h>

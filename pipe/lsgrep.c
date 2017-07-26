b60bd066eda2b72b52477a906d6c43742ad113db
Yusuke Ikezaki <y.ikezaki.287@ms.saitama-u.ac.jp>
2017-07-26 14:57:57 +0900

    edit lsgrep.c

--- pipe/lsgrep.c
+++ pipe/lsgrep.c
@@ -7,7 +7,7 @@
 #define BUFSIZE 64
 
 int main(int args, char *argv[]){
-  char str[] = "out.txt";
+  char filename[] = "out.txt";
   pid_t pid;
   int status;
   int fd[2];
@@ -25,12 +25,12 @@ int main(int args, char *argv[]){
   pid = fork();
   if(pid == 0){
     close(fd[1]);
-    read(fd[0], str, sizeof(str));
+    read(fd[0], filename, sizeof(filename));
     close(fd[0]);
-    execlp("grep", "grep", argv[2], str, NULL);
+    execlp("grep", "grep", argv[2], filename, NULL);
   } else if(pid > 0){
     close(fd[0]);
-    if((fd[1] = open("out.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1){
+    if((fd[1] = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1){
       perror("open failed");
       exit(EXIT_FAILURE);
     }
@@ -46,7 +46,7 @@ int main(int args, char *argv[]){
     pid = fork();
     if(pid == 0)
       execlp("ls", "ls", "-l", argv[1], NULL);
-    write(fd[1], str, sizeof(str));
+    write(fd[1], filename, sizeof(filename));
     close(fd[1]);
     wait(&status);
   } else{

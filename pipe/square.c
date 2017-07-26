8024088d049b1cd950398fa13855c266906e9d2d
Yusuke Ikezaki <y.ikezaki.287@ms.saitama-u.ac.jp>
2017-07-26 16:12:25 +0900

    edit files

--- pipe/square.c
+++ pipe/square.c
@@ -5,30 +5,51 @@
 #include <unistd.h>
 
 int main(){
-  int n;
+  /* 整数値 */
+  long int n;
+  /* 双方向パイプ */
   int fd1[2], fd2[2];
+  /* プロセスID */
   pid_t pid;
 
+  /* 双方向パイプの作成 */
   if((pipe(fd1) < 0) || (pipe(fd2) < 0)){
+    /* 作成失敗 */
     perror("pipe");
     exit(EXIT_FAILURE);
   }
+
+  /* フォーク */
   pid = fork();
   if(pid < 0){
+    /* フォーク失敗 */
     perror("fork");
     exit(EXIT_FAILURE);
   }
 
   if(pid == 0){
+    /* 子プロセス */
+    /* fd1の書き込みを閉じる */
     close(fd1[1]);
+    /* fd2の読み込みを閉じる */
     close(fd2[0]);
+
+    /* fd1からの読み込み */
     read(fd1[0], &n, sizeof(n));
+    /* fd1の読み込みを閉じる */
     close(fd1[0]);
-    printf("child : %d * %d = %d\n", n, n, n * n);
-    int squared = n * n;
+
+    /* 読み込んだ整数値の2乗を出力 */
+    long int squared = n * n;
+    printf("child : %d * %d = %d\n", n, n, squared);
+
+    /* fd2への書き込み */
     write(fd2[1], &squared, sizeof(n));
+    /* fd2の書き込みを閉じる */
     close(fd2[1]);
   } else{
+    /* 親プロセス */
+    /* fd1の読み込みを閉じる */
     close(fd1[0]);
     close(fd2[1]);
     printf("Parent > ");

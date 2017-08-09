9d44cbedb3b160d914b17bff0fea5a07965d02fd
Yusuke Ikezaki <iappley08@gmail.com>
2017-08-10 01:20:57 +0900

    simple version

---
+++ chat/client.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define BUFSIZE 256
+#define SEND 0
+#define RECV 1
+#define PARENT 2
+
+void chop(char *str){
+  char *p = strchr(str, '\n');
+  if(p != NULL)
+    *p = '\0';
+}
+
+int main(int argc, char *argv[]){
+  /* socket()の返すファイル識別子 */
+  int socket_fd;
+  /* サーバプロセスのソケットアドレス情報 */
+  struct sockaddr_in server;
+  /* ホスト情報 */
+  struct hostent *hp;
+  /* ポート番号 */
+  uint16_t port;
+  /* メッセージを格納するバッファ */
+  char buffer[BUFSIZE];
+  pid_t pid[PARENT];
+  int status;
+  int i;
+
+  /* 引数の確認 */
+  if(argc != 3){
+    /* 引数が正しくない */
+    fprintf(stderr, "Usage: %s <hostname> <port>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  /* ポート番号の設定 */
+  port = atoi(argv[2]);
+
+  /* ソケットの作成 */
+  socket_fd = socket(PF_INET, SOCK_STREAM, 0);
+  if(socket_fd == -1){
+    /* 作成失敗 */
+    perror("client: socket");
+    exit(EXIT_FAILURE);
+  }
+
+  /* サーバプロセスのソケットアドレス情報の設定 */
+  /* アドレス情報構造体の初期化 */
+  memset((void *) &server, 0, sizeof(server));
+  /* プロトコルファミリの設定 */
+  server.sin_family = PF_INET;
+  /* ポート番号の設定 */
+  server.sin_port = htons(port);
+
+  /* argv[1]のマシンのIPアドレスを返す */
+  if((hp = gethostbyname(argv[1])) == NULL){
+    /* 取得失敗 */
+    perror("client: gethostbyname");
+    exit(EXIT_FAILURE);
+  }
+  /* IPアドレスの設定 */
+  memcpy(&server.sin_addr, hp -> h_addr_list[0], hp -> h_length);
+
+  /* サーバに接続する */
+  if(connect(socket_fd, (struct sockaddr *) &server, sizeof(server)) == -1){
+    /* 接続失敗 */
+    perror("client: connect");
+    exit(EXIT_FAILURE);
+  }
+
+  for(i = 0; i < PARENT && (pid[i] = fork()) > 0; i++);
+
+  if(pid[i] == 0){
+    /* サーバとの通信 */
+    if(i == SEND){
+      /* 受信用 */
+      while(1){
+	memset(buffer, '\0', BUFSIZE);
+	recv(socket_fd, buffer, BUFSIZE, 0);
+	printf("\nfrom server: %s\n", buffer);
+	if(strcmp(buffer, "quit") == 0)
+	  break;
+	printf("> ");
+      }
+    } else if(i == RECV){
+      /* 送信用 */
+      while(1){
+	memset(buffer, '\0', BUFSIZE);
+	printf(">>> ");
+	if(fgets(buffer, BUFSIZE, stdin) == NULL)
+	  strcpy(buffer, "quit");
+	chop(buffer);
+	send(socket_fd, buffer, BUFSIZE, 0);
+	if(strcmp(buffer, "quit") == 0)
+	  break;
+      }
+    }
+    exit(0);
+  } else{
+    while(waitpid(-1, NULL, WNOHANG) <= 0);
+    for(i = 0; i < PARENT; i++)
+      kill(pid[i], SIGINT);
+  }
+
+  /* socket_fdを閉じる */
+  close(socket_fd);
+
+  return 0;
+}
---
+++ chat/server.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define BUFSIZE 256
+#define SEND 0
+#define RECV 1
+#define PARENT 2
+
+void chop(char *str){
+  char *p = strchr(str, '\n');
+  if(p != NULL)
+    *p = '\0';
+}
+
+int main(int argc, char *argv[]){
+  /* socket()が返すファイル識別子 */
+  int listening_socket;
+  /* accept()が返すファイル識別子 */
+  int connected_socket;
+  /* サーバプロセスのソケットアドレス情報 */
+  struct sockaddr_in server;
+  /* クライアントプロセスのソケットアドレス情報 */
+  struct sockaddr_in client;
+  /* クライアントプロセスのソケットアドレス情報の長さ */
+  socklen_t fromlen;
+  /* ポート番号 */
+  uint16_t port;
+  /* メッセージを格納するバッファ */
+  char buffer[BUFSIZE];
+  /* ソケットオプション用 */
+  int temp = 1;
+  pid_t pid[PARENT];
+  int i;
+
+  /* 引数の確認 */
+  if(argc != 2){
+    /* 引数が正しくない */
+    fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  /* ポート番号の設定 */
+  port = atoi(argv[1]);
+
+  /* ソケットの作成 */
+  listening_socket = socket(PF_INET, SOCK_STREAM, 0);
+  if(listening_socket == -1){
+      /* 作成失敗 */
+    perror("server: socket");
+    exit(EXIT_FAILURE);
+  }
+
+  /* ソケットオプションの設定 */
+  if(setsockopt(listening_socket, SOL_SOCKET, 
+		SO_REUSEADDR, (void *) &temp, sizeof(temp))){
+    /* 設定失敗 */
+    perror("server: setsockopt");
+    exit(EXIT_FAILURE);
+  }
+
+  /* サーバプロセスのソケットアドレス情報の設定 */
+  /* アドレス情報構造体の初期化 */
+  memset((void *) &server, 0, sizeof(server));
+  /* プロトコルファミリの設定 */
+  server.sin_family = PF_INET;
+  /* ポート番号の設定 */
+  server.sin_port = htons(port);
+  /* IPアドレスの設定 */
+  server.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  /* ソケットにアドレスをバインド */
+  if(bind(listening_socket, (struct sockaddr *) &server, sizeof(server)) == -1){
+    /* バインド失敗 */
+    perror("server: bind");
+      exit(EXIT_FAILURE);
+  }
+
+  /* 接続要求の受け入れ準備 */
+  if(listen(listening_socket, 5) == -1){
+    /* 準備失敗 */
+    perror("server: listen");
+    exit(EXIT_FAILURE);
+  }
+
+  /* クライアントプロセスのソケットアドレス情報の設定 */
+  /* アドレス情報構造体の初期化 */
+  memset((void *) &client, 0, sizeof(client));
+  /* ソケットアドレス情報の長さを取得 */
+  fromlen = sizeof(client);
+  /* クライアントからの接続要求を受け入れ、通信経路を確保する */
+  connected_socket = accept(listening_socket, 
+			    (struct sockaddr *) &client, &fromlen);
+  if(connected_socket == -1){
+    /* 受け入れ失敗 */
+    perror("server: accept");
+    exit(1);
+  }
+
+  /* listening_socketを閉じる */
+  close(listening_socket);
+
+  for(i = 0; i < PARENT && (pid[i] = fork()) > 0; i++);
+
+  if(pid[i] == 0){
+    /* クライアントとの通信 */
+    if(i == SEND){
+      /* 送信用 */
+      while(1){
+	memset(buffer, '\0', BUFSIZE);
+	printf(">>> ");
+	if(fgets(buffer, BUFSIZE, stdin) == NULL)
+	  strcpy(buffer, "quit");
+	chop(buffer);
+	send(connected_socket, buffer, BUFSIZE, 0);
+	if(strcmp(buffer, "quit") == 0)
+	  break;
+      }
+    } else if(i == RECV){
+      /* 受信用 */
+      while(1){
+	memset(buffer, '\0', BUFSIZE);
+	recv(connected_socket, buffer, BUFSIZE, 0);
+	printf("\nfrom client: %s\n> ", buffer);
+	if(strcmp(buffer, "quit") == 0)
+	  break;
+      }
+    }
+    exit(0);
+  } else{
+    while(waitpid(-1, NULL, WNOHANG) <= 0);
+    for(i = 0; i < PARENT; i++)
+      kill(pid[i], SIGINT);
+  }
+
+  /* connected_socketを閉じる */
+  close(connected_socket);
+
+  return 0;
+}
